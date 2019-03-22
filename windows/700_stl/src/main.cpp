72c7714f5fa152abac9a6fb047acb2902eb0dc0c
joshuaholloway <jhollow6@asu.edu>
2019-03-22 11:20:18 -0700

    lru

--- windows/700_stl/src/main.cpp
+++ windows/700_stl/src/main.cpp
@@ -1,7 +1,39 @@
 #include <vector>
 #include <algorithm>
 #include <iostream>
+typedef unsigned int uint;
+// ----------------------------------------------
+struct Min_Max_Struct
+{
+	uint min_idx, min_val;
+	uint max_idx, max_val;
+};
+// ----------------------------------------------
+Min_Max_Struct f(std::vector<int> x)
+{
+	Min_Max_Struct min_max;
+	min_max.min_val = x[0], min_max.min_idx = 0;
+	for (uint i = 0; i < x.size(); ++i)
+	{
+		if (x[i] < min_max.min_val)
+		{
+			min_max.min_val = x[i];
+			min_max.min_idx = i;
+		}
+	}
 
+	min_max.max_val = x[0], min_max.max_idx = 0;
+	for (uint i = 0; i < x.size(); ++i)
+	{
+		if (x[i] > min_max.max_val)
+		{
+			min_max.max_val = x[i];
+			min_max.max_idx = i;
+		}
+	}
+	return min_max;
+}
+// ------------------------------------------
 int main()
 {
 	using std::vector;
@@ -18,23 +50,30 @@ int main()
 	// Step 2: Place cache-line corresponding to min value
 	// Step 3: Make min value equal to the max-value possible and decrement all others
 
-	auto minmax = std::minmax_element(x.begin(), x.end());
 
-	std::cout << "minimum element: " << *minmax.first << '\n';
-	std::cout << "maximum element: " << *minmax.second << '\n';
+	for (uint i = 0; i < x.size(); i++)
+	{
+		auto min_max_struct = f(x);
+		//cout << "min-element's index: " << Min_Max_Struct.idx << "\n";
+		//cout << "min-element's value:  " << Min_Max_Struct.val << "\n";
+		//cout << "max-element's index: " << max_struct.idx << "\n";
+		//cout << "max-element's value:  " << max_struct.val << "\n";
 
-	int min_index = *minmax.first;
+		//std::cout << "Decrement all elements:\n";
+		for (auto& v : x)
+			--v;
+		//for (auto v : x)
+		//	cout << "x = " << v << "\n";
+		//cout << "\n";
 
-	x[*minmax.first] = x[*minmax.second];
+		//std::cout << "Minimum element becomes maximum possible.\n";
+		x[min_max_struct.min_idx] = min_max_struct.max_val;
 
-	for (auto& v : x)
-		--v;
-
-	x[min_index] = 3;
-
-	for (auto v : x)
-		cout << "x = " << v << "\n";
-	cout << "\n";
+		std::cout << "After run-" << i << " final result:\n";
+		for (auto v : x)
+			cout << "x = " << v << "\n";
+		cout << "\n";
+	}
 
 	getchar();
 	return 0;
